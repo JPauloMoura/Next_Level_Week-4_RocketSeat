65623fad0323c104035d9486ac5049e1c192207f
jpaulo-moura <joaopaulo.034a@gmail.com>
2021-02-25 00:49:12 -0300

    refactor(routes): adicionando novas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+import { SurveyController } from './controllers/SurveyController'
 import { UserController } from './controllers/UserController'
 
 const route = Router()
 
 const userController = new UserController()
+const surveyController = new SurveyController()
 
 route.post('/users', userController.create)
+route.post('/surveys', surveyController.create)
+route.get('/surveys', surveyController.show)
 
 export {route}
