14d5e88c5cd8cec68949749d7af80c8902908881
jpaulo-moura <joaopaulo.034a@gmail.com>
2021-02-25 00:46:55 -0300

    refactor(server): separando a configuração e execução do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,4 @@
-import 'reflect-metadata' //deve ser importado primeiro
-import express, {Express, Request, Response} from 'express'
-import './database'
-import { route } from './routes'
-
-const app: Express = express()
-app.use(express.json()) // habilitando o express trabalhar com JSON
-app.use(route) // arquivo de rotas
+import { app } from "./app"
 
 app.listen(3333, ()=>{
     console.log("---> SERVIDOR RODANDO!!!!")
