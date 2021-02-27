4bcf3bf9898805d784697659d113a2405a30c886
JPauloMoura <joaopaulo.034a@gmail.com>
2021-02-27 20:09:22 -0300

    feat(errors): criando erros customizados

--- src/controllers/SendEmailController.ts
+++ src/controllers/SendEmailController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { resolve } from 'path'
 import { getCustomRepository } from "typeorm";
+import { AppError } from "../errors/App.Erros";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
@@ -18,38 +19,35 @@ class SendEmailController{
     //verificar se o usuario existe
     const userAlreadyExists = await usersRepository.findOne({email})
     if(!userAlreadyExists){
-      return res.status(401).json({
-        error: 'User does not exists'
-      })
+      throw new AppError('User does not exists', 401)
     }
 
     //verificar se a pesquisa existe
     const suveysAlreadyExists = await surveysRepository.findOne({id: survey_id})
     if(!suveysAlreadyExists){
-      return res.status(401).json({
-        error: 'Survey does not exists'
-      })
+      throw new AppError('Survey does not exists', 401)
     }
 
     // buscando o caminho do arquivo de email
     const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs') 
 
+    //verificar se um usuario já possui uma pesquisa cadastrada para ele
+    const suveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: {user_id: userAlreadyExists.id, value: null}, //filtra todos os usuario pelo id e value passados da tabela surveys_users
+      relations: ['user', 'survey'] // retorna as relações da tabela
+    })
+
     const variables = {
       name: userAlreadyExists.name,
       title: suveysAlreadyExists.title,
       description: suveysAlreadyExists.description,
-      user_id: userAlreadyExists.id,
+      id: '',
       link: process.env.URL_MAIL
     }
 
-    //verificar se um usuario já possui uma pesquisa cadastrada para ele
-    const suveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [{user_id: userAlreadyExists.id}, {value: null}], //filtra todos os usuario pelo id e value passados da tabela surveys_users
-      relations: ['user', 'survey'] // retorna as relações da tabela
-    })
-
     // caso essa pesquisa já exista, não precisa cadastrar outra, basta fazer o envio da que já existe
     if(suveyUserAlreadyExists){
+      variables.id = suveyUserAlreadyExists.id
       await SendEmailService.execute(email, suveysAlreadyExists.title, variables, npsPath)
       return res.status(201).json(suveyUserAlreadyExists)
     }
@@ -59,6 +57,10 @@ class SendEmailController{
 
     //salvando no banco de dados
     await surveysUsersRepository.save(surveyUser)
+
+    variables.id = surveyUser.id
+
+    await SendEmailService.execute(email, suveysAlreadyExists.title, variables, npsPath)
 
     return res.status(201).json(surveyUser)
   }
